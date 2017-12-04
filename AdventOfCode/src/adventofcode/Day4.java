2b8917a1aac4e74f99435411ddfa83e293fcf945
trgoodwin <>
2017-12-04 15:53:04 -0500

    day 4 code start

--- AdventOfCode/src/adventofcode/Day4.java
+++ AdventOfCode/src/adventofcode/Day4.java
@@ -1,5 +1,11 @@
 package adventofcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day4 {
     
+    public static void main(String args) {
+        List<String> passphrase = new ArrayList<>();
+    }
 }
